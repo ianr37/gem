92260e0b0ba2f311c91a16f40de583c07020b441
Ian Cairns <ian>
2021-03-23 09:43:00 +0000

    Added skeleton Flask instance.

---
+++ config.py
@@ -0,0 +1,31 @@
+
+import os
+import uuid
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+class Configuration(object):
+    SECRET_KEY = os.environ.get('MARQUEE_SECRET_KEY') or str(uuid.uuid4())
+
+    @staticmethod
+    def init_application(application):
+        pass
+
+class DevelopmentConfiguration(Configuration):
+    DEBUG = True
+
+class TestingConfiguration(Configuration):
+    TESTING = True
+
+class ProductionConfiguration(Configuration):
+    pass
+
+configuration = {
+    'development': DevelopmentConfiguration(),
+    'testing': TestingConfiguration(),
+    'production': ProductionConfiguration(),
+}
+
+def get_configuration(name):
+    return configuration.get(name, DevelopmentConfiguration())
+
---
+++ entry_points/website.py
@@ -0,0 +1,14 @@
+
+import os
+
+from flask import Flask, render_template
+
+app = Flask('gem', static_url_path='')
+config_name = os.environ.get('FLASK_ENV') or 'development'
+from config import get_configuration
+app.config.from_object(get_configuration(config_name))
+
+@app.route("/", methods=['GET'])
+def render_root_page():
+    return render_template('index.html')
+
