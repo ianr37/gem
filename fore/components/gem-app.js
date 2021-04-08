06cd9c0b6ef5fa679808981cf1a976482a3f82cc
Ian Cairns <ian>
2021-04-08 11:14:25 +0100

    Rearranged desktop layout to use grid.

--- fore/components/gem-app.js
+++ fore/components/gem-app.js
@@ -4,40 +4,38 @@ const template = `
         <header>
             <h1>Application Header</h1>
         </header>
+        <nav>
+          <img src="logo.png"/>
+          <menu>
+              <button>Nav 1</button>
+              <button>Nav 2</button>
+              <button>Nav 3</button>
+              <button>Nav 4</button>
+              <button>Nav 5</button>
+          </menu>
+        </nav>
         <main>
-            <nav>
-              <img src="logo.png"/>
+          <menu>
+              <button>Menu 1</button>
+              <button>Menu 2</button>
+              <button>Menu 3</button>
+              <button>Menu 4</button>
+              <button>Menu 5</button>
+          </menu>
+          <article>
+              <h2>Article header</h2>
+              <label>User Name</label>
+              <input type="text" name="username"/>
+              <label>Password</label>
+              <input type="password" name="password"/>
               <menu>
-                  <button>Nav 1</button>
-                  <button>Nav 2</button>
-                  <button>Nav 3</button>
-                  <button>Nav 4</button>
-                  <button>Nav 5</button>
+                  <input type="submit"/>
               </menu>
-            </nav>
-            <article>
-              <menu>
-                  <button>Menu 1</button>
-                  <button>Menu 2</button>
-                  <button>Menu 3</button>
-                  <button>Menu 4</button>
-                  <button>Menu 5</button>
-              </menu>
-              <section>
-                  <h2>Article header</h2>
-                  <label>User Name</label>
-                  <input type="text" name="username"/>
-                  <label>Password</label>
-                  <input type="password" name="password"/>
-                  <menu>
-                      <input type="submit"/>
-                  </menu>
-              </section>
-            </article>
-            <aside>
-                <h2>Aside</h2>
-            </aside>
+          </article>
         </main>
+        <aside>
+            <h2>Aside</h2>
+        </aside>
         <footer>
           <span>&#169; Your Name Here</span>
         </footer>
--- fore/gem.js
+++ fore/gem.js
@@ -5,6 +5,5 @@ import './logo.png';
 import {GemApp} from './components';
 window.customElements.define('gem-app', GemApp, {extends: 'div'});
 const app = document.createElement('div', {is: 'gem-app'});
-app.classList.add('vbox');
 document.body.appendChild(app);
 
