2ebc749360c70281d0784bb6d2e3554147a67c39
R37 Developer <r37dev@iancairns.org>
2021-05-25 13:43:51 +0100

    Reworked running of workflows. Two tests (run-pause) omitted.

---
+++ fore/domain/workflow-step.mjs
@@ -0,0 +1,43 @@
+export class WorkflowStep {
+
+    constructor(name, taskName, jumps) {
+        this.name = name;
+        this.taskName = taskName;
+        this.jumps = new Map();
+    }
+
+    addJump(stateName, stepName) {
+        this.jumps.set(stateName, stepName);
+    }
+
+    deleteJump(stateName) {
+        this.jumps.delete(stateName);
+    }
+
+}
+
+export class WorkflowStepFactory {
+
+    createStep(definition) {
+        const step = new WorkflowStep();
+        if ('name' in definition) {
+            step.name = definition.name;
+        } else {
+            throw new Error('WorkflowStepFactory: step name not given');
+        }
+        if ('task' in definition) {
+            step.taskName = definition.task;
+        } else {
+            throw new Error('WorkflowStepFactory: task name not given');
+        }
+        if ('jumps' in definition) {
+            for (const [i, jump] of definition.jumps.entries()) {
+                step.addJump(jump.state, jump.target);
+            }
+        } else {
+            throw new Error('WorkflowStepFactory: jumps not given');
+        }
+        return step;
+    }
+
+}
