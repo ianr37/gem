2add0eb79b2882c74a375ae3cc626ec1da567e5b
R37 Developer <r37dev@r37.co.uk>
2021-06-12 09:07:24 +0100

    Moved workflow classes to fore/domain/workflows subfolder.

--- fore/domain/index.mjs
+++ fore/domain/index.mjs
@@ -2,9 +2,5 @@
 export * from './driver-action.mjs';
 export * from './form.mjs';
 export * from './user-profile.mjs';
-export * from './workflow.mjs';
-export * from './workflow-parameter.mjs';
-export * from './workflow-step.mjs';
-export * from './workflow-store.mjs';
-export * from './workflow-task.mjs';
+export * from './workflows/index.mjs';
 
---
+++ fore/domain/workflows/index.mjs
@@ -0,0 +1,7 @@
+
+export * from './workflow.mjs';
+export * from './workflow-parameter.mjs';
+export * from './workflow-step.mjs';
+export * from './workflow-store.mjs';
+export * from './workflow-task.mjs';
+
