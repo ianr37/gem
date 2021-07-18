194742e6f9b1e43d9e7f787ac118904a7c474420
R37 Developer <r37dev@r37.co.uk>
2021-07-18 05:49:50 +0100

    Move workflow related stuff frpm MvcController to DesktopController.

--- fore/adapters/desktop-controller.mjs
+++ fore/adapters/desktop-controller.mjs
@@ -4,7 +4,12 @@ import { MvcController } from '../domain/index.mjs';
 export class DesktopController extends MvcController {
 
     constructor(workflowStore, workflowFactory) {
-        super(workflowStore, workflowFactory);
+        super();
+        this.workflowStore = workflowStore;
+        this.workflowFactory = workflowFactory;
+        this.activeWorkflows = new Map();
+        this.keepFinishedWorkflows = false;
+        this.finishedWorkflows = new Map();
     }
 
     handleAction(action) {
@@ -22,6 +27,44 @@ export class DesktopController extends MvcController {
                 break;
             default:
                 throw new Error(`Controller#handleAction: unknown action ${action.action}`);
+                break;
+        }
+    }
+    
+    startWorkflow(parameters) {
+        const definition = this.workflowStore.getDefinition(parameters.name);
+        if (! definition) {
+            throw new Error(`Controller#startWorkflow: unknown workflow ${parameters.name}`);
+        }
+        const workflow = this.workflowFactory.createWorkflow(definition, this);
+        if (! workflow) {
+            throw new Error(`Controller#startWorkflow: unknown workflow ${parameters.name}`);
+        }
+        this.activeWorkflows.set(workflow.flowId, workflow);
+        this.executeWorkflow(workflow, null);
+    }
+
+    resumeWorkflow(parameters) {
+        this.executeWorkflow(parameters.workflow, parameters.result);
+    }
+
+    executeWorkflow(workflow, previousOutcome) {
+        const result = workflow.execute(previousOutcome);
+        switch (result.status) {
+            case 'paused':
+                break;
+            case 'finished':
+                if (this.keepFinishedWorkflows) {
+                    this.finishedWorkflows.set(workflow.flowId, workflow);
+                }
+                this.activeWorkflows.delete(workflow.flowId);
+                break;
+            case 'failed':
+                this.finishedWorkflows.set(workflow.flowId, workflow);
+                this.activeWorkflows.delete(workflow.flowId);
+                break;
+            default:
+                throw new Error(`Unexpected step status "${result.state}"`);
                 break;
         }
     }
--- fore/domain/mvc-controller.mjs
+++ fore/domain/mvc-controller.mjs
@@ -1,56 +1,14 @@
 
 export class MvcController {
 
-    constructor(workflowStore, workflowFactory) {
+    constructor() {
         this.model = null;
         this.view = null;
-        this.workflowStore = workflowStore;
-        this.workflowFactory = workflowFactory;
-        this.activeWorkflows = new Map();
-        this.keepFinishedWorkflows = false;
-        this.finishedWorkflows = new Map();
     }
 
     handleAction(action) {
         throw new Error('NotImplemented');
     }
 
-    startWorkflow(parameters) {
-        const definition = this.workflowStore.getDefinition(parameters.name);
-        if (! definition) {
-            throw new Error(`Controller#startWorkflow: unknown workflow ${parameters.name}`);
-        }
-        const workflow = this.workflowFactory.createWorkflow(definition, this);
-        if (! workflow) {
-            throw new Error(`Controller#startWorkflow: unknown workflow ${parameters.name}`);
-        }
-        this.activeWorkflows.set(workflow.flowId, workflow);
-        this.executeWorkflow(workflow, null);
-    }
-
-    resumeWorkflow(parameters) {
-        this.executeWorkflow(parameters.workflow, parameters.result);
-    }
-
-    executeWorkflow(workflow, previousOutcome) {
-        const result = workflow.execute(previousOutcome);
-        switch (result.status) {
-            case 'paused':
-                break;
-            case 'finished':
-                if (this.keepFinishedWorkflows) {
-                    this.finishedWorkflows.set(workflow.flowId, workflow);
-                }
-                this.activeWorkflows.delete(workflow.flowId);
-                break;
-            case 'failed':
-                this.finishedWorkflows.set(workflow.flowId, workflow);
-                this.activeWorkflows.delete(workflow.flowId);
-                break;
-            default:
-                throw new Error(`Unexpected step status "${result.state}"`);
-                break;
-        }
-    }
 }
 
